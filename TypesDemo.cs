4f98134ff302ac8f602ba7ebf2e83db8b4bc0639
sujit Bhosale <163643360+jit8484@users.noreply.github.com>
2025-06-09 22:27:13 +0530

    Add files via upload

---
+++ TypesDemo.cs
@@ -0,0 +1,22 @@
+using System;
+class TypesDemo
+{
+static int i;
+static void Main()
+{
+int j=1;
+
+Console.WriteLine("Value of i is:"+i);
+Consol e.WriteLine("type of i is :"+i.getType());
+Console.WriteLine("Value of j is :"+j);
+Console.WriteLine("type of j is :"+j.getType());
+  
+
+float f=3.14f;
+Console.WriteLine("type of f is :" +f.getType());
+double d=3.14;
+Console.WriteLine("type of d is :" + d.getType());
+
+     
+}
+}   
