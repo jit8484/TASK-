ea724ac6203ef2393ea53fa64679bd8d481f4731
sujit Bhosale <163643360+jit8484@users.noreply.github.com>
2025-06-09 22:25:56 +0530

    Add files via upload

---
+++ Thrid.cs
@@ -0,0 +1,19 @@
+
+/* To test the process of importing a class write below code in Notepad and execute:
+6
+6.6
+6.36
+6
+3
+6
+*/
+using static System. Console;
+class Third
+{
+static void Main()
+
+{
+Clear();
+WriteLine("importing a type.");
+}
+}
